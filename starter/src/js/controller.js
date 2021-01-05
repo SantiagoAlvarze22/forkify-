f2c282dc5fe29f90362ca2c9fa3a41ed318c55e5
Santiago Alvarez Olivera <sao2207@gmail.com>
2021-01-05 12:11:45 -0500

    implementing pagination pt2

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 //importing regenerator
 import 'core-js/stable';
@@ -48,13 +49,26 @@ const controlSearchResults = async function () {
 
     // 3) render results
     resultsView.render(model.getSearchResultPage());
+
+    //4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  model.getSearchResultPage(goToPage);
+  // 3) render NEW results
+  resultsView.render(model.getSearchResultPage(goToPage));
+
+  //4) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
