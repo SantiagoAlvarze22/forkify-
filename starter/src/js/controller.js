2b195a17dd184976129a8ed0caebf3877c605995
Santiago Alvarez Olivera <sao2207@gmail.com>
2021-01-06 21:43:17 -0500

    uploading a new recipe pt3

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -116,6 +116,12 @@ const controlAddRecipe = async function (newRecipe) {
     //success message
     addRecipeView.renderMessage();
 
+    //render bookmar view
+    bookmarksView.render(model.state.bookmarks);
+
+    //change ID in URL, with history web- API -change the URL without reloing the page
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
     //close form window
     setTimeout(function () {
       addRecipeView.toggleWindow();
